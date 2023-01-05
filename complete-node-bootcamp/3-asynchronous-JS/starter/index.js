476713e079c3f1cec92f295b2cbcca7e0f743ad2
adrierjs <adrierj@gmail.com>
2023-01-05 00:57:57 -0300

    Do calbeck para as promess

--- complete-node-bootcamp/3-asynchronous-JS/starter/index.js
+++ complete-node-bootcamp/3-asynchronous-JS/starter/index.js
@@ -5,8 +5,7 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     if (err) return console.log(err.message)
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        if (err) return console.log(err.message);
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then((res) => { //Como se fosse um if
 
         console.log(res.header.date);
         console.log(res.body.message);
@@ -15,5 +14,7 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
             console.log("Arquivo de texto gravado com sucesso!")
         })
 
+    }).catch((err) =>{ // Como se fosse um else
+        console.log(err.message);
     })
 })
