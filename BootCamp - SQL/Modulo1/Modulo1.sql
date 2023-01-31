cd5f5206537226dd78baf292fe80445ee85b51e6
adrierjs <adrierj@gmail.com>
2023-01-31 00:02:29 -0300

    utilizando distinct, count e select combinados

--- BootCamp - SQL/Modulo1/Modulo1.sql
+++ BootCamp - SQL/Modulo1/Modulo1.sql
@@ -1,1 +1,17 @@
-select * from film where film_id between '1' and '20'
+with total as (
+	
+	select actor_id, first_name, last_name, last_update from actor 
+	where actor_id 
+	between '1' and '10'
+	
+	order by 1
+	asc)
+
+		 
+-- select count(actor_id) as quantidade from total;
+select distinct * from total;
+	
+	
+	
+	
+	
