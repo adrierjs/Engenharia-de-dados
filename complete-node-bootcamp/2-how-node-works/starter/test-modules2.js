91358c4c19692f94ea539447d55448cbf75df41a
Adrier José <80015771+adrierjs@users.noreply.github.com>
2024-07-19 23:06:05 -0300

    Delete complete-node-bootcamp directory

--- complete-node-bootcamp/2-how-node-works/starter/test-modules2.js
+++
@@ -1,3 +0,0 @@
-exports.adicao = (a,b) => `A soma é ${a+b}`;
-exports.divisao = (a,b) => a/b;
-exports.multi = (a,b) => a*b;
--- complete-node-bootcamp/2-how-node-works/starter/test-modules3.js
+++
@@ -1,3 +0,0 @@
-console.log("Olá");
-module.exports = () => console.log("Testando módulos");
-module.exports= (nome) => console.log(`Olá ${nome}`);
